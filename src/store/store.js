12d5e25e06d13c6038a2b9adc7e5d1ac4fe2bc73
MohammedAzhar043 <146701964+MohammedAzhar043@users.noreply.github.com>
2025-07-09 22:18:09 -0700

    managing loaders and error scenarios

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { productReducer } from "./reducers/productReducer";
+import { errorReducer } from "./reducers/errorReducer";
 
 export const store = configureStore({
-  reducer: { products: productReducer },
+  reducer: {
+    products: productReducer,
+    errors: errorReducer,
+  },
   preloadedState: {},
 });
 
