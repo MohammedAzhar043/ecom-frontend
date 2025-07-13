8d06e628990ae5ecab5ee0a100881fb5dda92939
MohammedAzhar043 <146701964+MohammedAzhar043@users.noreply.github.com>
2025-07-13 04:02:42 -0700

    added or setting upa navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Products from "./components/products/Products";
 import Home from "./components/home/home";
+import Navbar from "./components/shared/Navbar";
 
 function App() {
   return (
     <>
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
            <Route path="/products" element={<Products />} />
