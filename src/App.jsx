8e2b37bb5916d598199e15b8fa114abfe0fd3e19
MohammedAzhar043 <146701964+MohammedAzhar043@users.noreply.github.com>
2025-07-12 23:56:48 -0700

    setting up routig in the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Products from "./components/products/Products";
-
+import Home from "./components/home/home";
 
 function App() {
   return (
     <>
-       <Products/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+           <Route path="/products" element={<Products />} />
+        </Routes>
+      </Router>
     </>
   );
 }
