0c0eb026896b8c2a8061941bdd80b84afc9b79a8
MohammedAzhar043 <146701964+MohammedAzhar043@users.noreply.github.com>
2025-07-14 07:20:02 -0700

    setting up redux for user authentication

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,18 @@ import { configureStore } from "@reduxjs/toolkit";
 import { productReducer } from "./reducers/productReducer";
 import { errorReducer } from "./reducers/errorReducer";
 import { createReducer } from "./reducers/cartReducer";
+import { authReducer } from "./reducers/authReducer";
 
 const cartItems = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
 
+const user = localStorage.getItem("auth")
+  ? JSON.parse(localStorage.getItem("auth"))
+  : [];
+
 const initialState = {
+  auth: { user: user },
   carts: { cart: cartItems },
 };
 
@@ -16,6 +22,7 @@ export const store = configureStore({
     products: productReducer,
     errors: errorReducer,
     carts: createReducer,
+    auth: authReducer,
   },
   preloadedState: initialState,
 });
