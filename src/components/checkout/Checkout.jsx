18bd9f2283cc0e13629d842c2e05a5b16fb9be87
MohammedAzhar043 <146701964+MohammedAzhar043@users.noreply.github.com>
2025-07-16 03:24:11 -0700

    fetching user addresses from backend

--- src/components/checkout/Checkout.jsx
+++ src/components/checkout/Checkout.jsx
@@ -1,10 +1,19 @@
 import { Step, StepLabel, Stepper } from "@mui/material";
 import React, { useState } from "react";
 import AddressInfo from "./AddressInfo";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getUserAddresses } from "../../store/action";
 
 const Checkout = () => {
   const [activeStep, setActiveStep] = useState(0);
   const steps = ["Address", "Payment Method", "Order Summary", "Payment"];
+
+  const dispatch = useDispatch();
+   
+  useEffect(()=>{
+      dispatch(getUserAddresses())
+  },[dispatch])
   return (
     <div className="py-14 min-h-[calc(100vh-100px)]">
       <Stepper activeStep={activeStep} alternativeLabel>
