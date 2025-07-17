6c288244181393569154a77650a3aff0f265039a
MohammedAzhar043 <146701964+MohammedAzhar043@users.noreply.github.com>
2025-07-16 21:49:59 -0700

    address selection :saving selected user address for checkout

--- src/components/checkout/AddressList.jsx
+++ src/components/checkout/AddressList.jsx
@@ -7,16 +7,17 @@ import {
   FaTrash,
 } from "react-icons/fa";
 import { MdLocationCity, MdPinDrop, MdPublic } from "react-icons/md";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { selectUserCheckoutAddress } from "../../store/action";
+
 
 const AddressList = ({
   addresses,
   setSelectedAddress,
   setOpenAddressModal,
 }) => {
-  const handleAddressSelection = (address) => {};
   const dispatch = useDispatch();
-  const selectedUserAddress = addresses[0];
+  const { selectedUserCheckoutAddress } = useSelector((state) => state.auth);
 
   const onEditButtonHandler = (addresses) => {
     setSelectedAddress(addresses);
@@ -27,6 +28,10 @@ const AddressList = ({
     setSelectedAddress(addresses);
     setOpenDeleteModal(true);
   };
+
+  const handleAddressSelection = (address) => {
+    dispatch(selectUserCheckoutAddress(address));
+  };
   return (
     <div className="space-y-4 ">
       {addresses.map((address) => (
@@ -34,7 +39,7 @@ const AddressList = ({
           key={address.addressId}
           onClick={() => handleAddressSelection(address)}
           className={`p-4 border rounded-md cursor-pointer relative ${
-            selectedUserAddress?.addressId === address.addressId
+            selectedUserCheckoutAddress?.addressId === address.addressId
               ? "bg-green-100"
               : "bg-white"
           }`}
@@ -44,7 +49,8 @@ const AddressList = ({
               <div className="flex items-center ">
                 <FaBuilding size={14} className="mr-2 text-gray-600" />
                 <p className="font-semibold">{address.buildingName}</p>
-                {selectedUserAddress?.addressId === address.addressId && (
+                {selectedUserCheckoutAddress?.addressId ===
+                  address.addressId && (
                   <FaCheckCircle className="text-green-500 ml-2" />
                 )}
               </div>
@@ -56,7 +62,9 @@ const AddressList = ({
 
               <div className="flex items-center ">
                 <MdLocationCity size={17} className="mr-2 text-gray-600" />
-                <p className="">{address.city}</p>
+                <p className="">
+                  {address.city} , {address.state}
+                </p>
               </div>
 
               <div className="flex items-center ">
@@ -79,9 +87,6 @@ const AddressList = ({
             </button>
           </div>
         </div>
-        
-        
-
       ))}
     </div>
   );
--- src/store/action/index.js
+++ src/store/action/index.js
@@ -197,3 +197,13 @@ export const getUserAddresses = () => async (dispatch, getState) => {
     });
   }
 };
+
+
+export const selectUserCheckoutAddress = (address) => {
+    // localStorage.setItem("CHECKOUT_ADDRESS", JSON.stringify(address));
+    
+    return {
+        type: "SELECT_CHECKOUT_ADDRESS",
+        payload: address,
+    }
+};
